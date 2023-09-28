44f4715251241dd8b08694a70ef551f0cdf1d2d0
Brett Jia <dev.bjia56@gmail.com>
2023-09-28 15:56:32 -0400

    update version string format

--- scripts/pick_semver.sh
+++ scripts/pick_semver.sh
@@ -5,16 +5,16 @@ python_version=$1
 tags=$(git tag | grep $python_version)
 
 if [[ -z "$tags" ]]; then
-    echo "v$python_version-0"
+    echo "v$python_version-build.0"
     exit 0
 fi
 
 most_recent=0
 while read tag; do
-    tag_postfix=$(echo $tag | sed "s/^v$python_version-//")
+    tag_postfix=$(echo $tag | sed "s/^v$python_version-build\.//")
     if [ "$tag_postfix" -gt "$most_recent" ]; then
         most_recent=$tag_postfix
     fi
 done <<< "$tags"
 
-echo "v$python_version-$(expr $most_recent + 1)"
+echo "v$python_version-build.$(expr $most_recent + 1)"
