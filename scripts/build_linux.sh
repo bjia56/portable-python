8d215906c058106ff0cb4baf8f0ef5f412a7038a
Brett Jia <dev.bjia56@gmail.com>
2023-12-13 15:07:57 -0500

    turn on verbose mode

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -86,7 +86,7 @@ if [[ "${ARCH}" == "armv7l" ]]; then
   additionalparams+=(-DUSE_SYSTEM_LIBMPDEC=ON)
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 fi
-cmake \
+cmake --trace-expand \
   -DPYTHON_VERSION=${PYTHON_FULL_VER} \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DCMAKE_INSTALL_PREFIX:PATH=${PWD}/../python-install \
