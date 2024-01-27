63f533da80b2a2a9e37a615ab7af09c1610bf8a2
Brett Jia <dev.bjia56@gmail.com>
2024-01-27 08:10:01 -0500

    arm fixes

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -24,17 +24,14 @@ case "$ARCH" in
   x86_64)
     apt -y install libc6-amd64-cross
     ln -s /usr/x86_64-linux-gnu/lib/ld-linux-x86-64.so.2 /lib/ld-linux-x86-64.so.2
-    export TARGET=${ARCH}-linux-gnu.2.17
     ;;
   aarch64)
     apt -y install libc6-arm64-cross
     ln -s /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 /lib/ld-linux-aarch64.so.1
-    export TARGET=${ARCH}-linux-gnu.2.17
     ;;
   arm)
     apt -y install libc6-armhf-cross
     ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
-    export TARGET=${ARCH}-linux-gnueabihf.2.17
     ;;
 esac
 
@@ -54,7 +51,14 @@ export CC=zig_cc
 export CXX=zig_cxx
 export CHOST=${ARCH}
 
-export ZIG_TARGET=${TARGET}
+case "$ARCH" in
+  arm)
+    export ZIG_TARGET=${ARCH}-linux-gnueabihf.2.17
+    ;;
+  *)
+    export ZIG_TARGET=${ARCH}-linux-gnu.2.17
+    ;;
+esac
 
 echo "::endgroup::"
 ########
@@ -82,7 +86,14 @@ wget -q https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_1_1_1w.tar.
 tar -xf OpenSSL*.tar.gz
 rm *.tar.gz
 cd openssl-OpenSSL*
-./Configure linux-${ARCH} no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+case "$ARCH" in
+  arm)
+    ./Configure linux-generic32 no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+    ;;
+  *)
+    ./Configure linux-${ARCH} no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+    ;;
+esac
 make -j4
 make install_sw
 
