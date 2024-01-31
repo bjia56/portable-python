9df23e6502b80603d6d9bf564cb016543f577c09
Brett Jia <dev.bjia56@gmail.com>
2024-01-30 23:33:22 -0500

    host parameterization

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -22,14 +22,20 @@ case "$ARCH" in
   x86_64)
     sudo apt -y install libc6-amd64-cross
     sudo ln -s /usr/x86_64-linux-gnu/lib/ld-linux-x86-64.so.2 /lib/ld-linux-x86-64.so.2
+    export ZIG_TARGET=${ARCH}-linux-gnu.2.17
+    export CHOST=${ARCH}-linux-gnu
     ;;
   aarch64)
     sudo apt -y install libc6-arm64-cross
     sudo ln -s /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 /lib/ld-linux-aarch64.so.1
+    export ZIG_TARGET=${ARCH}-linux-gnu.2.17
+    export CHOST=${ARCH}-linux-gnu
     ;;
   arm)
     sudo apt -y install libc6-armhf-cross
     sudo ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
+    export ZIG_TARGET=${ARCH}-linux-gnueabihf.2.17
+    export CHOST=${ARCH}-linux-gnueabihf
     ;;
 esac
 sudo pip install https://github.com/mesonbuild/meson/archive/2baae24.zip ninja
@@ -37,7 +43,6 @@ sudo pip install https://github.com/mesonbuild/meson/archive/2baae24.zip ninja
 mkdir ${BUILDDIR}
 mkdir ${DEPSDIR}
 
-export ZIG_TARGET=${ARCH}-linux-gnu.2.17
 
 # Python's sysconfig module will retain references to these compiler values, which cause
 # problems when sysconfig is used to pick a compiler during binary extension builds.
@@ -50,7 +55,6 @@ sudo cp ${WORKDIR}/zigshim/zig_cxx /usr/bin/${ARCH}-linux-gnu-g++
 export AR="${ARCH}-linux-gnu-gcc-ar"
 export CC="${ARCH}-linux-gnu-gcc"
 export CXX="${ARCH}-linux-gnu-g++"
-export CHOST=${ARCH}
 export CFLAGS="-I${DEPSDIR}/include"
 export CPPFLAGS="-I${DEPSDIR}/include"
 export CXXFLAGS="${CPPFLAGS}"
@@ -79,7 +83,11 @@ cd ${BUILDDIR}
 
 download_verify_extract openssl-1.1.1w.tar.gz
 cd openssl*
-./Configure linux-${ARCH} no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+if [[ "${ARCH}" == "arm" ]]; then
+  ./Configure linux-${ARCH}-generic32 no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+else
+  ./Configure linux-${ARCH} no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+fi
 make -j4
 make install_sw
 
@@ -92,7 +100,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libffi-3.4.2.tar.gz
 cd libffi*
-./configure --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -105,7 +113,7 @@ cd ${BUILDDIR}
 
 download_verify_extract sqlite-autoconf-3450000.tar.gz
 cd sqlite*
-./configure --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -118,7 +126,7 @@ cd ${BUILDDIR}
 
 download_verify_extract expat-2.5.0.tar.gz
 cd expat*
-./configure --host=${ARCH}-linux --disable-shared --prefix=${DEPSDIR}
+./configure --host=${CHOST} --disable-shared --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -131,7 +139,7 @@ cd ${BUILDDIR}
 
 download_verify_extract ncurses-6.4.tar.gz
 cd ncurses*
-./configure --host=${ARCH}-linux --with-normal --without-progs --enable-overwrite --disable-stripping --prefix=${DEPSDIR}
+./configure --host=${CHOST} --with-normal --without-progs --enable-overwrite --disable-stripping --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -144,7 +152,7 @@ cd ${BUILDDIR}
 
 download_verify_extract readline-8.2.tar.gz
 cd readline*
-./configure --with-curses --disable-shared --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --with-curses --disable-shared --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -205,7 +213,7 @@ cd ${BUILDDIR}
 download_verify_extract util-linux-2.39.3.tar.gz
 cd util-linux*
 ./autogen.sh
-./configure --host=${ARCH}-linux --disable-all-programs --enable-libuuid --prefix=${DEPSDIR}
+./configure --host=${CHOST} --disable-all-programs --enable-libuuid --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -218,7 +226,7 @@ cd ${BUILDDIR}
 
 download_verify_extract gdbm-1.23.tar.gz
 cd gdbm*
-./configure --host=${ARCH}-linux --enable-libgdbm-compat --prefix=${DEPSDIR}
+./configure --host=${CHOST} --enable-libgdbm-compat --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -231,7 +239,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libxml2-2.12.4.tar.xz
 cd libxml2*
-./configure --host=${ARCH}-linux --enable-static --disable-shared --without-python --prefix=${DEPSDIR}
+./configure --host=${CHOST} --enable-static --disable-shared --without-python --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -244,7 +252,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libpng-1.6.41.tar.gz
 cd libpng*
-./configure --host=${ARCH}-linux --with-zlib-prefix=${DEPSDIR} --disable-tools --prefix=${DEPSDIR}
+./configure --host=${CHOST} --with-zlib-prefix=${DEPSDIR} --disable-tools --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -257,7 +265,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libgpg-error-1.47.tar.bz2
 cd libgpg-error*
-./configure --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -265,7 +273,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libgcrypt-1.10.3.tar.bz2
 cd libgcrypt*
-LDFLAGS="${LDFLAGS} -Wl,--undefined-version" ./configure --disable-asm --host=${ARCH}-linux --prefix=${DEPSDIR}
+LDFLAGS="${LDFLAGS} -Wl,--undefined-version" ./configure --disable-asm --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -278,7 +286,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libxslt-1.1.39.tar.xz
 cd libxslt*
-CFLAGS="${CFLAGS} -I${DEPSDIR}/include/libxml2" ./configure --host=${ARCH}-linux --with-libxml-prefix=${DEPSDIR} --without-python --prefix=${DEPSDIR}
+CFLAGS="${CFLAGS} -I${DEPSDIR}/include/libxml2" ./configure --host=${CHOST} --with-libxml-prefix=${DEPSDIR} --without-python --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -291,7 +299,7 @@ cd ${BUILDDIR}
 
 download_verify_extract freetype-2.13.2.tar.gz
 cd freetype*
-./configure --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -304,7 +312,7 @@ cd ${BUILDDIR}
 
 download_verify_extract fontconfig-2.15.0.tar.gz
 cd fontconfig*
-LDFLAGS="${LDFLAGS} -lxml2" ./configure --host=${ARCH}-linux --enable-static --disable-shared --enable-libxml2 --disable-cache-build --prefix=${DEPSDIR}
+LDFLAGS="${LDFLAGS} -lxml2" ./configure --host=${CHOST} --enable-static --disable-shared --enable-libxml2 --disable-cache-build --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -325,7 +333,7 @@ function build_x11_lib () {
   download_verify_extract $file
   cd $pkg
   autoreconf -vfi
-  ./configure $ext_flags --host=${ARCH}-linux --prefix=${DEPSDIR}
+  ./configure $ext_flags --host=${CHOST} --prefix=${DEPSDIR}
   make -j4
   make install
 
@@ -362,7 +370,7 @@ cd ${BUILDDIR}
 
 download_verify_extract tcl8.6.13-src.tar.gz
 cd tcl*/unix
-LDFLAGS="${LDFLAGS} -lxml2" ./configure --disable-shared --host=${ARCH}-linux --prefix=${DEPSDIR}
+LDFLAGS="${LDFLAGS} -lxml2" ./configure --disable-shared --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -375,7 +383,7 @@ cd ${BUILDDIR}
 
 download_verify_extract tk8.6.13-src.tar.gz
 cd tk*/unix
-LDFLAGS="${LDFLAGS} -lxml2" ./configure --disable-shared --host=${ARCH}-linux --prefix=${DEPSDIR}
+LDFLAGS="${LDFLAGS} -lxml2" ./configure --disable-shared --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -388,7 +396,7 @@ cd ${BUILDDIR}
 
 download_verify_extract mpdecimal-2.5.0.tar.gz
 cd mpdecimal*
-./configure --disable-cxx --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --disable-cxx --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
