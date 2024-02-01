20ba87a247af5883bcd2b818f9fc9a452ccda9a8
Brett Jia <dev.bjia56@gmail.com>
2024-02-01 11:58:45 -0500

    Arm32 builds with Zig (#14)
    * try build arm with zig
    
    * disable all other builds for now
    
    * host parameterization
    
    * fix openssl platform
    
    * tweak flags and exports
    
    * add cpu arch to cflags
    
    * fix libffi error
    
    * try targeting armv6 pi
    
    * Revert "try targeting armv6 pi"
    
    This reverts commit ec6ff01d148b4c82e6e0f12827d30d4fbfe92487.
    
    * libmpdec
    
    * arm interpreter for cmake
    
    * Change armv7l to arm
    
    * More recent patchelf
    
    * sudo
    
    * Update build_linux_zig.sh
    
    * newer patchelf
    
    * back to older patchelf
    
    * target pi1
    
    * Tweak cpu
    
    * Try setting only rpath
    
    * Add back patches
    
    * Consolidate
    
    * make qemu patches consistent
    
    * rpath shenanigans
    
    * backup and restore patched program
    
    * use temporary patched emulator
    
    * likely don't need to patch anymore
    
    * downgrade patchelf
    
    * skip patch to test
    
    * use LD_LIBRARY_PATH in interpreter
    
    * restore patches
    
    * bring back full builds
    
    * simplify
    
    * remove unused
    
    * Update build_python.yml
    
    * arm's dynamic linking is busted and requires newer ldd
    
    * use correct parse
    
    * fix img parsing
    
    * back to centos7 for x64/arm64

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -6,7 +6,6 @@ source ${SCRIPT_DIR}/utils.sh
 
 zig version
 
-
 ########################
 # Install dependencies #
 ########################
@@ -16,7 +15,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt update
 sudo apt -y install \
   wget build-essential pkg-config cmake autoconf git \
-  python2 python3 python3-pip clang patchelf qemu-user-static \
+  python2 python3 python3-pip clang qemu-user-static \
   gettext bison libtool autopoint gperf ncurses-bin xutils-dev
 case "$ARCH" in
   x86_64)
@@ -32,30 +31,42 @@ case "$ARCH" in
     sudo ln -s /usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
     ;;
 esac
-sudo pip install https://github.com/mesonbuild/meson/archive/2baae24.zip ninja
+sudo pip install https://github.com/mesonbuild/meson/archive/2baae24.zip ninja patchelf==0.15.0.0
 
 mkdir ${BUILDDIR}
 mkdir ${DEPSDIR}
 
-export ZIG_TARGET=${ARCH}-linux-gnu.2.17
-
-# Python's sysconfig module will retain references to these compiler values, which cause
-# problems when sysconfig is used to pick a compiler during binary extension builds.
-# Since clang (zig) is a drop-in replacement for gcc, we set these so the final sysconfig
-# will work on other platforms.
-sudo cp ${WORKDIR}/zigshim/zig_ar /usr/bin/${ARCH}-linux-gnu-gcc-ar
-sudo cp ${WORKDIR}/zigshim/zig_cc /usr/bin/${ARCH}-linux-gnu-gcc
-sudo cp ${WORKDIR}/zigshim/zig_cxx /usr/bin/${ARCH}-linux-gnu-g++
-
-export AR="${ARCH}-linux-gnu-gcc-ar"
-export CC="${ARCH}-linux-gnu-gcc"
-export CXX="${ARCH}-linux-gnu-g++"
-export CHOST=${ARCH}
 export CFLAGS="-I${DEPSDIR}/include"
 export CPPFLAGS="-I${DEPSDIR}/include"
 export CXXFLAGS="${CPPFLAGS}"
 export LDFLAGS="-L${DEPSDIR}/lib"
 export PKG_CONFIG_PATH="${DEPSDIR}/lib/pkgconfig:${DEPSDIR}/share/pkgconfig"
+
+if [[ "${ARCH}" == "arm" ]]; then
+  # Python's sysconfig module will retain references to these compiler values, which cause
+  # problems when sysconfig is used to pick a compiler during binary extension builds.
+  # Since clang (zig) is a drop-in replacement for gcc, we set these so the final sysconfig
+  # will work on other platforms.
+  sudo cp ${WORKDIR}/zigshim/zig_ar /usr/bin/${ARCH}-linux-gnueabihf-gcc-ar
+  sudo cp ${WORKDIR}/zigshim/zig_cc /usr/bin/${ARCH}-linux-gnueabihf-gcc
+  sudo cp ${WORKDIR}/zigshim/zig_cxx /usr/bin/${ARCH}-linux-gnueabihf-g++
+  export AR="${ARCH}-linux-gnueabihf-gcc-ar"
+  export CC="${ARCH}-linux-gnueabihf-gcc"
+  export CXX="${ARCH}-linux-gnueabihf-g++"
+  export ZIG_TARGET=${ARCH}-linux-gnueabihf.2.17
+  export CHOST=${ARCH}-linux-gnueabihf
+  export CFLAGS="-mfpu=vfp -mfloat-abi=hard -mcpu=arm1176jzf_s ${CFLAGS}"
+else
+  # See above comment
+  sudo cp ${WORKDIR}/zigshim/zig_ar /usr/bin/${ARCH}-linux-gnu-gcc-ar
+  sudo cp ${WORKDIR}/zigshim/zig_cc /usr/bin/${ARCH}-linux-gnu-gcc
+  sudo cp ${WORKDIR}/zigshim/zig_cxx /usr/bin/${ARCH}-linux-gnu-g++
+  export AR="${ARCH}-linux-gnu-gcc-ar"
+  export CC="${ARCH}-linux-gnu-gcc"
+  export CXX="${ARCH}-linux-gnu-g++"
+  export ZIG_TARGET=${ARCH}-linux-gnu.2.17
+  export CHOST=${ARCH}-linux-gnu
+fi
 
 echo "::endgroup::"
 ########
@@ -79,7 +90,11 @@ cd ${BUILDDIR}
 
 download_verify_extract openssl-1.1.1w.tar.gz
 cd openssl*
-./Configure linux-${ARCH} no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+if [[ "${ARCH}" == "arm" ]]; then
+  ./Configure linux-generic32 no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+else
+  ./Configure linux-${ARCH} no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+fi
 make -j4
 make install_sw
 
@@ -92,7 +107,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libffi-3.4.2.tar.gz
 cd libffi*
-./configure --host=${ARCH}-linux --prefix=${DEPSDIR}
+CFLAGS="${CFLAGS} -Wl,--undefined-version" ./configure --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -105,7 +120,7 @@ cd ${BUILDDIR}
 
 download_verify_extract sqlite-autoconf-3450000.tar.gz
 cd sqlite*
-./configure --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -118,7 +133,7 @@ cd ${BUILDDIR}
 
 download_verify_extract expat-2.5.0.tar.gz
 cd expat*
-./configure --host=${ARCH}-linux --disable-shared --prefix=${DEPSDIR}
+./configure --host=${CHOST} --disable-shared --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -131,7 +146,7 @@ cd ${BUILDDIR}
 
 download_verify_extract ncurses-6.4.tar.gz
 cd ncurses*
-./configure --host=${ARCH}-linux --with-normal --without-progs --enable-overwrite --disable-stripping --prefix=${DEPSDIR}
+./configure --host=${CHOST} --with-normal --without-progs --enable-overwrite --disable-stripping --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -144,7 +159,7 @@ cd ${BUILDDIR}
 
 download_verify_extract readline-8.2.tar.gz
 cd readline*
-./configure --with-curses --disable-shared --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --with-curses --disable-shared --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -205,7 +220,7 @@ cd ${BUILDDIR}
 download_verify_extract util-linux-2.39.3.tar.gz
 cd util-linux*
 ./autogen.sh
-./configure --host=${ARCH}-linux --disable-all-programs --enable-libuuid --prefix=${DEPSDIR}
+./configure --host=${CHOST} --disable-all-programs --enable-libuuid --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -218,7 +233,7 @@ cd ${BUILDDIR}
 
 download_verify_extract gdbm-1.23.tar.gz
 cd gdbm*
-./configure --host=${ARCH}-linux --enable-libgdbm-compat --prefix=${DEPSDIR}
+./configure --host=${CHOST} --enable-libgdbm-compat --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -231,7 +246,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libxml2-2.12.4.tar.xz
 cd libxml2*
-./configure --host=${ARCH}-linux --enable-static --disable-shared --without-python --prefix=${DEPSDIR}
+./configure --host=${CHOST} --enable-static --disable-shared --without-python --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -244,7 +259,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libpng-1.6.41.tar.gz
 cd libpng*
-./configure --host=${ARCH}-linux --with-zlib-prefix=${DEPSDIR} --disable-tools --prefix=${DEPSDIR}
+./configure --host=${CHOST} --with-zlib-prefix=${DEPSDIR} --disable-tools --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -257,7 +272,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libgpg-error-1.47.tar.bz2
 cd libgpg-error*
-./configure --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -265,7 +280,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libgcrypt-1.10.3.tar.bz2
 cd libgcrypt*
-LDFLAGS="${LDFLAGS} -Wl,--undefined-version" ./configure --disable-asm --host=${ARCH}-linux --prefix=${DEPSDIR}
+LDFLAGS="${LDFLAGS} -Wl,--undefined-version" ./configure --disable-asm --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -278,7 +293,7 @@ cd ${BUILDDIR}
 
 download_verify_extract libxslt-1.1.39.tar.xz
 cd libxslt*
-CFLAGS="${CFLAGS} -I${DEPSDIR}/include/libxml2" ./configure --host=${ARCH}-linux --with-libxml-prefix=${DEPSDIR} --without-python --prefix=${DEPSDIR}
+CFLAGS="${CFLAGS} -I${DEPSDIR}/include/libxml2" ./configure --host=${CHOST} --with-libxml-prefix=${DEPSDIR} --without-python --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -291,7 +306,7 @@ cd ${BUILDDIR}
 
 download_verify_extract freetype-2.13.2.tar.gz
 cd freetype*
-./configure --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -304,7 +319,7 @@ cd ${BUILDDIR}
 
 download_verify_extract fontconfig-2.15.0.tar.gz
 cd fontconfig*
-LDFLAGS="${LDFLAGS} -lxml2" ./configure --host=${ARCH}-linux --enable-static --disable-shared --enable-libxml2 --disable-cache-build --prefix=${DEPSDIR}
+LDFLAGS="${LDFLAGS} -lxml2" ./configure --host=${CHOST} --enable-static --disable-shared --enable-libxml2 --disable-cache-build --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -325,7 +340,7 @@ function build_x11_lib () {
   download_verify_extract $file
   cd $pkg
   autoreconf -vfi
-  ./configure $ext_flags --host=${ARCH}-linux --prefix=${DEPSDIR}
+  ./configure $ext_flags --host=${CHOST} --prefix=${DEPSDIR}
   make -j4
   make install
 
@@ -362,7 +377,7 @@ cd ${BUILDDIR}
 
 download_verify_extract tcl8.6.13-src.tar.gz
 cd tcl*/unix
-LDFLAGS="${LDFLAGS} -lxml2" ./configure --disable-shared --host=${ARCH}-linux --prefix=${DEPSDIR}
+LDFLAGS="${LDFLAGS} -lxml2" ./configure --disable-shared --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -375,7 +390,7 @@ cd ${BUILDDIR}
 
 download_verify_extract tk8.6.13-src.tar.gz
 cd tk*/unix
-LDFLAGS="${LDFLAGS} -lxml2" ./configure --disable-shared --host=${ARCH}-linux --prefix=${DEPSDIR}
+LDFLAGS="${LDFLAGS} -lxml2" ./configure --disable-shared --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -388,7 +403,7 @@ cd ${BUILDDIR}
 
 download_verify_extract mpdecimal-2.5.0.tar.gz
 cd mpdecimal*
-./configure --disable-cxx --host=${ARCH}-linux --prefix=${DEPSDIR}
+./configure --disable-cxx --host=${CHOST} --prefix=${DEPSDIR}
 make -j4
 make install
 
@@ -398,6 +413,12 @@ echo "::endgroup::"
 ##########
 echo "::group::Python"
 cd ${BUILDDIR}
+
+additionalparams=()
+if [[ "${ARCH}" == "arm" ]]; then
+  additionalparams+=(-DUSE_SYSTEM_LIBMPDEC=ON)
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${DEPSDIR}/lib
+fi
 
 wget --no-verbose -O python-cmake-buildsystem.tar.gz https://github.com/bjia56/python-cmake-buildsystem/tarball/portable-python
 tar -xf python-cmake-buildsystem.tar.gz
@@ -420,6 +441,7 @@ LDFLAGS="${LDFLAGS} -lfontconfig -lfreetype" cmake \
   -DBUILD_TESTING=${INSTALL_TEST} \
   -DINSTALL_TEST=${INSTALL_TEST} \
   -DINSTALL_MANUAL=OFF \
+  "${additionalparams[@]}" \
   -DOPENSSL_INCLUDE_DIR:PATH=${DEPSDIR}/include \
   -DOPENSSL_LIBRARIES="${DEPSDIR}/lib/libssl.a;${DEPSDIR}/lib/libcrypto.a" \
   -DSQLite3_INCLUDE_DIR:PATH=${DEPSDIR}/include \
@@ -488,13 +510,11 @@ echo "::group::Check executable dependencies (post-patch)"
 cd ${BUILDDIR}
 
 cd python-install
-# we don't make ldd errors fatal here since ldd doesn't work
-# when cross compiling
 echo "python dependencies"
-ldd -v -r ./bin/python || true
+readelf -d ./bin/python
 echo
 echo "libpython dependencies"
-ldd -v -r ./lib/libpython${PYTHON_VER}.so || true
+readelf -d ./lib/libpython${PYTHON_VER}.so
 
 echo "::endgroup::"
 ###############
@@ -504,7 +524,7 @@ echo "::group::Test python"
 cd ${BUILDDIR}
 
 cd python-install
-qemu-${ARCH}-static ./bin/python --version
+${WORKDIR}/scripts/qemu_${ARCH}_interpreter ./bin/python --version
 
 echo "::endgroup::"
 ###############
@@ -514,7 +534,7 @@ echo "::group::Preload pip"
 cd ${BUILDDIR}
 
 cd python-install
-qemu-${ARCH}-static ./bin/python -m ensurepip
+${WORKDIR}/scripts/qemu_${ARCH}_interpreter ./bin/python -m ensurepip
 
 echo "::endgroup::"
 ###################
