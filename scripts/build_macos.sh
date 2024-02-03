07e906eff1c39b2e70333cd1dee362f211677de4
Brett Jia <dev.bjia56@gmail.com>
2024-02-03 00:12:22 -0500

    switch to python 3.11 dev branch

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -20,7 +20,7 @@ mkdir ${LICENSEDIR}
 
 export MACOSX_DEPLOYMENT_TARGET=10.5
 
-git clone https://github.com/bjia56/python-cmake-buildsystem.git --branch portable-python --single-branch --depth 1
+git clone https://github.com/bjia56/python-cmake-buildsystem.git --branch python3.11 --single-branch --depth 1
 
 echo "::endgroup::"
 ###########
--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -14,7 +14,7 @@ mkdir python-install
 mkdir deps
 mkdir ${LICENSEDIR}
 
-git clone https://github.com/bjia56/python-cmake-buildsystem.git --branch portable-python --single-branch --depth 1
+git clone https://github.com/bjia56/python-cmake-buildsystem.git --branch python3.11 --single-branch --depth 1
 
 echo "::endgroup::"
 ###########
