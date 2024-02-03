aa4ec8ac80d0e9e9fee6ecbed183be286cc771e5
Brett Jia <dev.bjia56@gmail.com>
2024-02-03 16:50:10 -0500

    target glibc 2.34

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -70,7 +70,7 @@ else
   export CC="${ARCH}-linux-gnu-gcc"
   export CXX="${ARCH}-linux-gnu-g++"
   if [[ "${ARCH}" == "riscv64" ]]; then
-    export ZIG_TARGET=riscv64-linux-gnu
+    export ZIG_TARGET=riscv64-linux-gnu.2.34
   else
     export ZIG_TARGET=${ARCH}-linux-gnu.2.17
   fi
