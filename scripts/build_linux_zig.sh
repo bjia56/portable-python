e83ff5e0b58f245b4ef8cf0f4d0170655b588559
Brett Jia <dev.bjia56@gmail.com>
2024-03-06 18:22:37 -0500

    point to sysconfig fix branch

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -500,7 +500,7 @@ if [[ "${ARCH}" == "arm" ]]; then
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${DEPSDIR}/lib
 fi
 
-wget --no-verbose -O python-cmake-buildsystem.tar.gz https://github.com/bjia56/python-cmake-buildsystem/tarball/portable-python
+wget --no-verbose -O python-cmake-buildsystem.tar.gz https://github.com/bjia56/python-cmake-buildsystem/tarball/sysconfig-fixes
 tar -xf python-cmake-buildsystem.tar.gz
 rm *.tar.gz
 mv *python-cmake-buildsystem* python-cmake-buildsystem
