8dae11e5ef19f668570ff39f61e706749c2049ae
Brett Jia <dev.bjia56@gmail.com>
2024-03-13 23:15:56 -0400

    use clang++

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -32,7 +32,7 @@ cd ${BUILDDIR}
 
 download_verify_extract ncurses-6.4.tar.gz
 cd ncurses*
-CC=clang CFLAGS="${CFLAGS} -arch x86_64 -arch arm64" ./configure --with-normal --without-progs --enable-overwrite --disable-stripping --prefix=${DEPSDIR}
+CC=clang CXX=clang++ CFLAGS="${CFLAGS} -arch x86_64 -arch arm64" CXXFLAGS="${CXXFLAGS} -arch x86_64 -arch arm64" ./configure --with-normal --without-progs --enable-overwrite --disable-stripping --prefix=${DEPSDIR}
 make -j4
 make install
 install_license
