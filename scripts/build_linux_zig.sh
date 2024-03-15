6d6b3d6236cd4fd2a1751de5a3b9fdbc21742b31
Brett Jia <dev.bjia56@gmail.com>
2024-03-14 23:57:12 -0400

    move lp64 patching of libpython into qemu shim

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -550,15 +550,6 @@ LDFLAGS="${LDFLAGS} -lfontconfig -lfreetype" cmake \
 make -j4
 make install
 
-if [[ "${ARCH}" == "riscv64" ]]; then
-  cd ${BUILDDIR}/python-install
-  # Zig incorrectly links the binary with and sets the interpreter to ld-linux-riscv64-lp64.so.1,
-  # so we have to correct it.
-  # Since we will run the compiled Python through the qemu shim, we rely on the shim to fix the
-  # main interpreter executable. However, we still have to fix libpython here.
-  patchelf --replace-needed ld-linux-riscv64-lp64.so.1 ld-linux-riscv64-lp64d.so.1 ./lib/libpython${PYTHON_VER}.so
-fi
-
 cd ${BUILDDIR}
 cp -r ${DEPSDIR}/lib/tcl8.6 ./python-install/lib
 cp -r ${DEPSDIR}/lib/tk8.6 ./python-install/lib
