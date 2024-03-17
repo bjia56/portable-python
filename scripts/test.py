32bd508bb39809dbaa6b9e05c03590382bbc2310
Brett Jia <dev.bjia56@gmail.com>
2024-03-17 18:53:19 -0400

    Revert "test import every module"
    This reverts commit 06b24da9f160dc9bcf0e9ead83f7560e4fe75250.

--- scripts/test.py
+++ scripts/test.py
@@ -1,16 +1,9 @@
-# test importing every module
-import pkgutil
-import traceback
-all_mods = [m.name for m in pkgutil.iter_modules()]
-print("All modules: ", all_mods)
-for m in all_mods:
-    try:
-        __import__(m)
-        print("Imported: ", m)
-    except Exception as e:
-        print("Failed to import: ", m)
-        print(traceback.format_exc())
+# test ssl is linked correctly
+import ssl
 
 # test urllib can use the bundled certifi CAs
 import urllib.request
 urllib.request.urlopen('https://github.com')
+
+# test ctypes is linked correctly
+import ctypes
