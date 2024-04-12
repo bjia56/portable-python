1c609e3d5cf19a6d3a3a555b9fb46b80736cd627
Brett Jia <dev.bjia56@gmail.com>
2024-04-12 12:55:01 -0400

    use cross compiler ld

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -98,6 +98,7 @@ else
     export CFLAGS="-Wl,--undefined-version ${CFLAGS}"
   elif [[ "${ARCH}" == "s390x" ]]; then
     export ZIG_FLAGS="-target s390x-linux-gnu.2.19 -mcpu=z196"
+    export LD=s390x-linux-gnu-ld
   else
     export ZIG_FLAGS="-target ${ARCH}-linux-gnu.2.17"
   fi
