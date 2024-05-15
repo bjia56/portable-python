2ea945a29b2ac1842c1e0b4fa6206009b8fc42bb
Brett Jia <dev.bjia56@gmail.com>
2024-05-15 16:04:47 -0400

    fix missing else clause

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -143,6 +143,7 @@ elif [[ "${ARCH}" == "i386" ]]; then
   ./Configure linux-x86 no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
 elif [[ "${ARCH}" == "riscv64" ]]; then
   CFLAGS="${CFLAGS} -fgnuc-version=0 -D__STDC_NO_ATOMICS__" ./Configure linux-generic64 no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+else
   ./Configure linux-${ARCH} no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
 fi
 make -j4
