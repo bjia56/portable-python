ff546c158e9012d20c60fd9488537149ddb1f2be
Brett Jia <dev.bjia56@gmail.com>
2024-06-10 00:43:22 -0400

    More DL_ARCH

--- scripts/repackage_graalpy.sh
+++ scripts/repackage_graalpy.sh
@@ -11,11 +11,12 @@ fi
 
 WORKDIR=$(pwd)
 
-curl -L https://github.com/oracle/graalpython/releases/download/graal-${GRAALPY_VERSION}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz --output graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.tar.gz
-tar -xf graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.tar.gz
-cd graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}
+curl -L https://github.com/oracle/graalpython/releases/download/graal-${GRAALPY_VERSION}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz --output graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz
+tar -xf graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz
+cd graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}
 ./bin/python -m ensurepip
 
+mv graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH} graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}
 tar -czf ${WORKDIR}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.tar.gz graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}
 if [[ "${PLATFORM}" == "windows" ]]; then
   7z.exe a ${WORKDIR}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.zip graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}
