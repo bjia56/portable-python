be5ab76de2b110f860de0ee901283b8fa80036ad
Brett Jia <dev.bjia56@gmail.com>
2024-06-10 15:27:31 -0400

    use zip on windows

--- scripts/repackage_graalpy.sh
+++ scripts/repackage_graalpy.sh
@@ -11,16 +11,20 @@ fi
 
 WORKDIR=$(pwd)
 
-curl -L https://github.com/oracle/graalpython/releases/download/graal-${GRAALPY_VERSION}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz --output graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz
-tar -xf graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz
+if [[ "${PLATFORM}" == "windows" ]]; then
+  curl -L https://github.com/oracle/graalpython/releases/download/graal-${GRAALPY_VERSION}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.zip --output graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.zip
+  7z.exe x graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.zip
+else
+  curl -L https://github.com/oracle/graalpython/releases/download/graal-${GRAALPY_VERSION}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz --output graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz
+  tar -xf graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}.tar.gz
+fi
+
 cd graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH}
 ./bin/python -m ensurepip
 
-mv graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${DL_ARCH} graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}
 tar -czf ${WORKDIR}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.tar.gz graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}
 if [[ "${PLATFORM}" == "windows" ]]; then
   7z.exe a ${WORKDIR}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.zip graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}
 else
-  tar -czf ${WORKDIR}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.tar.gz graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}
   zip ${WORKDIR}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.zip $(tar tf ${WORKDIR}/graalpy-community-${GRAALPY_VERSION}-${PLATFORM}-${ARCH}.tar.gz)
 fi
