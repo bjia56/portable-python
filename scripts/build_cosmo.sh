39073a514fe3d061a6b05193abeb39b5ec0ca173
Brett Jia <dev.bjia56@gmail.com>
2024-06-29 22:30:30 -0400

    tweaks

--- scripts/build_cosmo.sh
+++ scripts/build_cosmo.sh
@@ -4,8 +4,6 @@ PLATFORM=cosmo
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source ${SCRIPT_DIR}/utils.sh
 
-ARCH=unknown
-
 ########################
 # Install dependencies #
 ########################
