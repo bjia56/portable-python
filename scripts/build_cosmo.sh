92fd368cb29e097eb936cbd9796553236b5ff2cd
Brett Jia <dev.bjia56@gmail.com>
2024-06-29 21:15:39 -0400

    expand multi-os test

--- scripts/build_cosmo.sh
+++ scripts/build_cosmo.sh
@@ -4,6 +4,8 @@ PLATFORM=cosmo
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source ${SCRIPT_DIR}/utils.sh
 
+ARCH=unknown
+
 ########################
 # Install dependencies #
 ########################
