33596af6e97a774f6dc61d4048fa57da3db2b8c2
Brett Jia <dev.bjia56@gmail.com>
2024-06-30 23:22:27 -0400

    Revert "maybe we don't need libpython"
    This reverts commit 0f6a615272572f73d103b8e3c799ea472735298c.

--- scripts/build_cosmo.sh
+++ scripts/build_cosmo.sh
@@ -220,8 +220,7 @@ rm ./python-install/bin/python${PYTHON_VER}
 if [[ "${DEBUG_CI}" == "true" ]]; then
   cp ./python-build/bin/python.com.dbg ./python-install/bin/
 fi
-rm ./python-install/lib/libpython*.a
-#cp -r ./python-build/lib/.aarch64 ./python-install/lib/
+cp -r ./python-build/lib/.aarch64 ./python-install/lib/
 cp -r ${LICENSEDIR} ./python-install
 
 echo "::endgroup::"
