018307ca45edbf9ebbcbd3a76b8362a326b14473
Brett Jia <dev.bjia56@gmail.com>
2024-07-01 13:30:09 -0400

    Add Cosmopolitan libc builds (#65)
    * let's see how this goes
    
    * add action
    
    * chmod +x
    
    * syntax
    
    * remove unused
    
    * static only
    
    * static
    
    * openssl tweaks
    
    * libffi tweak
    
    * libffi tweaks
    
    * build separate libffis
    
    * use arch specific ar
    
    * log
    
    * Revert "log"
    
    This reverts commit cb756d68e34da970f17758e50a02617890a0d9f0.
    
    * merge libffi archives
    
    * patch configure script
    
    * disable shared
    
    * more sed
    
    * Revert "Revert "log""
    
    This reverts commit 6929b91fae65b3c89bba32e8b18e726437f06f7a.
    
    * Revert "Revert "Revert "log"""
    
    This reverts commit b591554467729bf82fb39537127271405d5661dd.
    
    * cmake overrides
    
    * Add quotes
    
    * disable shared
    
    * cmake tweaks
    
    * Drop libuuid
    
    * Disable shared
    
    * Copy aarch64 libs
    
    * Copy some libs
    
    * Copy more libs
    
    * Disable shared
    
    * Disable some asm
    
    * more build tweaks
    
    * try cosmoinstall
    
    * Revert "try cosmoinstall"
    
    This reverts commit a81b0015fa955b1c7cae482d1b190db39f700be4.
    
    * build libgcrypt separately
    
    * set host
    
    * Use aarch64 lib dir
    
    * Fix out dir
    
    * Force lib dir again
    
    * skip gpg-error-config
    
    * more specific aarch64 copys
    
    * fix libfreetype path
    
    * add libbrotlicommon
    
    * Revert "add libbrotlicommon"
    
    This reverts commit a292665cf4fb2e0aae74ba097874c28f126edb8b.
    
    * wip workflow refactoring
    
    * add libbrotlicommon
    
    * expand multi-os test
    
    * wf tweak
    
    * pin action
    
    * disable fc-cache
    
    * modify freetype pkgconfig
    
    * fix copy
    
    * disable x11/tcl/tk, tweak python build
    
    * disable docs
    
    * tweak job name
    
    * tweaks
    
    * disable linux extensions
    
    * link with expat
    
    * use cmake's cosmocc checks to disable extensions
    
    * add libexpat
    
    * cleanup + disable pip stuff for now
    
    * wf tweaks
    
    * Use extension
    
    * Update build_python.yml
    
    * Update build_cosmo.sh
    
    * Update build_python.yml
    
    * Add cosmo
    
    * Add cosmo
    
    * remove linux arm64 test
    
    * remove macos x86_64 because it requires billing
    
    * disable ctypes
    
    * patch pip script
    
    * use feature branch
    
    * pyclean cosmo
    
    * copy debug symbols into result
    
    * Revert "disable x11/tcl/tk, tweak python build"
    
    This reverts commit c368f80b1f556307aab0821103aa744d8f9022d5.
    
    * copy aarch64 for proper libs
    
    * only build cosmo
    
    * Revert "copy aarch64 for proper libs"
    
    This reverts commit b171de922e9102e7abcbbeb3f2e7ca2460ebf3c3.
    
    * Revert "Revert "disable x11/tcl/tk, tweak python build""
    
    This reverts commit a381548b27246438cffd8ee86e9a6e8d1c1b7fd2.
    
    * probably don't need these libs if x11 isn't going to build
    
    * Remove unused
    
    * Remove unused
    
    * Remove unused
    
    * remove more unused
    
    * test cosmo on freebsd
    
    * Revert "test cosmo on freebsd"
    
    This reverts commit d7fdc3e2481fc222bb7115ef4db2cc4d966934d4.
    
    * use older cosmocc
    
    * Revert "Revert "test cosmo on freebsd""
    
    This reverts commit f59c328321a857d5f22e9018d8b637bb09b15bc1.
    
    * fix freebsd test
    
    * only save python.com
    
    * fix script path
    
    * use older cosmocc
    
    * only include dbg when in debug build
    
    * copy aarch64 libpython
    
    * bring back all builds
    
    * maybe we don't need libpython
    
    * Revert "maybe we don't need libpython"
    
    This reverts commit 0f6a615272572f73d103b8e3c799ea472735298c.
    
    * Use latest cosmocc with windows fix
    
    * Test NetBSD and OpenBSD
    
    * Split out
    
    * full path
    
    * install unzip
    
    * bash -e
    
    * add cosmopolitan license
    
    * fix dir
    
    * bump cosmocc, fix openbsd pkgs
    
    * Revert "remove more unused"
    
    This reverts commit 289213d1c8c29e8555f6cc90ece094cc90b4a847.
    
    * poll subprocess in test
    
    * update comment

--- scripts/patch_pip_script.py
+++ scripts/patch_pip_script.py
@@ -3,22 +3,26 @@ import sys
 # adapted from https://stackoverflow.com/a/57567228
 new_shebang = """#!/bin/sh
 "true" '''\\'
-exec "$(dirname "$0")"/python "$0" "$@"
+exec "$(dirname "$0")"/python{} "$0" "$@"
 '''"""
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <pip_script>")
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <pip_script> [<suffix>]")
         sys.exit(1)
 
     with open(sys.argv[1], "r") as f:
         lines = f.readlines()
 
+    suffix = ''
+    if len(sys.argv) == 3:
+        suffix = sys.argv[2]
+
     script = ""
     for line in lines:
         if line.startswith("#!"):
             # replace the shebang of the script with a relative path to the python interpreter
-            script += new_shebang + "\n"
+            script += new_shebang.format(suffix) + "\n"
         else:
             script += line
 
