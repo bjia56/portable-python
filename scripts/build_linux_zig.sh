8b503d5ee0a20fb95adfa29bfcbb521039a85cc3
Brett Jia <dev.bjia56@gmail.com>
2024-07-28 20:18:22 -0400

    use development build of zig

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -97,7 +97,7 @@ else
     export ZIG_FLAGS="-target riscv64-linux-gnu.2.27"
     export CFLAGS="-Wl,--undefined-version ${CFLAGS}"
   elif [[ "${ARCH}" == "s390x" ]]; then
-    export ZIG_FLAGS="-target s390x-linux-gnu.2.19 -mcpu=z16"
+    export ZIG_FLAGS="-target s390x-linux-gnu.2.19"
   else
     export ZIG_FLAGS="-target ${ARCH}-linux-gnu.2.17"
   fi
