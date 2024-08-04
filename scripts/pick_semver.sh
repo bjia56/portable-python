7c0030b7117ac16119ed3dd0931189434bc766f2
Brett Jia <dev.bjia56@gmail.com>
2024-08-03 20:47:16 -0400

    graalpy release job + prefix releases with implementation

--- scripts/pick_semver.sh
+++ scripts/pick_semver.sh
@@ -2,15 +2,17 @@
 
 python_version=$1
 beta=$2
-print_latest=$3
+implementation=$3
+print_latest=$4
 
+prefix=$implementation
 
 suffix=build
 if [[ "$beta" == "true" ]]; then
     suffix=beta
 fi
 
-tags=$(git tag | grep "$python_version-$suffix")
+tags=$(git tag | grep "$prefix-v$python_version-$suffix")
 
 if [[ -z "$tags" ]]; then
     if [[ "$print_latest" != "true" ]]; then
@@ -21,14 +23,14 @@ fi
 
 most_recent=0
 while read tag; do
-    tag_postfix=$(echo $tag | sed "s/^v$python_version-$suffix\.//")
+    tag_postfix=$(echo $tag | sed "s/^$prefix-v$python_version-$suffix\.//")
     if [ "$tag_postfix" -gt "$most_recent" ]; then
         most_recent=$tag_postfix
     fi
 done <<< "$tags"
 
 if [[ "$print_latest" == "true" ]]; then
-    echo "v$python_version-$suffix.$most_recent"
+    echo "$prefix-v$python_version-$suffix.$most_recent"
 else
-    echo "v$python_version-$suffix.$(expr $most_recent + 1)"
+    echo "$prefix-v$python_version-$suffix.$(expr $most_recent + 1)"
 fi
