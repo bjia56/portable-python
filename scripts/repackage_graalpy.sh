d1fa56ce6e89ec2f21a3d3d70e1e77ab620b4b4c
Brett Jia <dev.bjia56@gmail.com>
2024-08-03 21:07:07 -0400

    add curl

--- scripts/repackage_graalpy.sh
+++ scripts/repackage_graalpy.sh
@@ -10,7 +10,7 @@ if [[ "${ARCH}" == "x86_64" ]]; then
 fi
 
 if [[ "${PLATFORM}" == "linux" ]]; then
-  apt update && apt -y install zip python3
+  apt update && apt -y install zip python3 curl
 fi
 
 WORKDIR=$(pwd)
