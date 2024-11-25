45059f27838dff151bd16881791b01e9652f197c
Brett Jia <dev.bjia56@gmail.com>
2024-11-24 21:40:40 -0500

    configure 32 bit openssl

--- scripts/build_solaris.sh
+++ scripts/build_solaris.sh
@@ -66,7 +66,11 @@ else
   download_verify_extract openssl-3.0.15.tar.gz
 fi
 cd openssl*
-./Configure solaris64-x86_64-gcc no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+if [[ "${ARCH}" == "i386" ]]; then
+  ./Configure solaris-x86-gcc no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+else
+  ./Configure solaris64-x86_64-gcc no-shared --prefix=${DEPSDIR} --openssldir=${DEPSDIR}
+fi
 gmake -j4
 gmake install_sw
 install_license
