1c2da17fd4792507c65e52168073901e8a4da906
Brett Jia <dev.bjia56@gmail.com>
2024-12-01 22:06:35 -0500

    fix file names

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -117,14 +117,14 @@ fi
 cd ${WORKDIR}
 
 if [[ "${ARCH}" == "powerpc64le" ]]; then
-  # Compile gcc_qadd, gcc_qmul manually and bundle them as libzigshim.a
+  # Compile the following manually and bundle them as libzigshim.a
   # This is needed because the Zig compiler does not yet support powerpc64le's double double
   # floating point format. https://github.com/ziglang/zig/issues/22081
   cd ${BUILDDIR}
   ${CC} -c -o gcc_qadd.o ${WORKDIR}/zigshim/gcc_qadd.c
   ${CC} -c -o gcc_qmul.o ${WORKDIR}/zigshim/gcc_qmul.c
-  ${CC} -c -o gcc_qmul.o ${WORKDIR}/zigshim/gcc_qsub.c
-  ${CC} -c -o gcc_qmul.o ${WORKDIR}/zigshim/gcc_qdiv.c
+  ${CC} -c -o gcc_qsub.o ${WORKDIR}/zigshim/gcc_qsub.c
+  ${CC} -c -o gcc_qdiv.o ${WORKDIR}/zigshim/gcc_qdiv.c
   ${AR} rcs libzigshim.a gcc_qadd.o gcc_qmul.o gcc_qsub.o gcc_qdiv.o
   mkdir -p ${DEPSDIR}/lib
   mv libzigshim.a ${DEPSDIR}/lib
