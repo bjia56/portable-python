a3e3ea9283b09d7637aa2a0dec6f05b1ba7a3b8d
Brett Jia <dev.bjia56@gmail.com>
2024-12-14 14:00:13 -0500

    try linker flag

--- scripts/build_linux_zig.sh
+++ scripts/build_linux_zig.sh
@@ -87,7 +87,7 @@ elif [[ "${ARCH}" == "i386" ]]; then
   export CFLAGS="-m32 -Wl,--undefined-version ${CFLAGS}"
   export CPPFLAGS="-m32 ${CPPFLAGS}"
   export CXXFLAGS="-m32 ${CXXFLAGS}"
-  export LDFLAGS="-m32 ${LDFLAGS}"
+  export LDFLAGS="-m32 -Wl,-z,notext ${LDFLAGS}"
 else
   # See above comment
   sudo cp ${WORKDIR}/zigshim/zig_ar /usr/bin/${ARCH}-linux-gnu-gcc-ar
