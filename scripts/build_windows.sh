07349c59f093b50aa6d5d53e891750332f3d75ea
Brett Jia <dev.bjia56@gmail.com>
2025-05-30 11:22:11 -0400

    switch over to arm64

--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -4,6 +4,14 @@ PLATFORM=windows
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source ${SCRIPT_DIR}/utils.sh
 
+if [ "${ARCH}" = "x86_64" ]; then
+  CMAKE_ARCH=x64
+  BINDEPS_ARCH=amd64
+else
+  CMAKE_ARCH=ARM64
+  BINDEPS_ARCH=arm64
+fi
+
 ##############
 # Initialize #
 ##############
@@ -20,33 +28,28 @@ echo "::group::OpenSSL"
 cd ${BUILDDIR}
 
 if (( ${PYTHON_MINOR} < 11 )); then
-  curl -L https://github.com/Slicer/Slicer-OpenSSL/releases/download/1.1.1g/OpenSSL_1_1_1g-install-msvc1900-64-Release.tar.gz --output openssl.tar.gz
-  tar -xf openssl.tar.gz
-  mv OpenSSL_1_1_1g-install-msvc1900-64-Release ${DEPSDIR}/openssl
-  mkdir openssl-1.1.1g
-  cd openssl-1.1.1g
-  curl -L https://www.openssl.org/source/license-openssl-ssleay.txt --output LICENSE
-  install_license
+  OPENSSL_VER=1.1.1u
 else
-  curl -L https://github.com/python/cpython-bin-deps/archive/refs/tags/openssl-bin-3.0.15.tar.gz --output cpython-bin-deps-openssl-bin-3.0.15.tar.gz
-  tar -xf cpython-bin-deps-openssl-bin-3.0.15.tar.gz
-  cd cpython-bin-deps-openssl-bin-3.0.15
-  mkdir ${DEPSDIR}/openssl
-  cp -r amd64/include ${DEPSDIR}/openssl/include
-  # cmake apparently wants this here?
-  cp ${DEPSDIR}/openssl/include/applink.c ${DEPSDIR}/openssl/include/openssl/applink.c
-
-  # required by cmake
-  mkdir ${DEPSDIR}/openssl/lib
-  cp amd64/lib* ${DEPSDIR}/openssl/lib/
-
-  # for compatibility with old openssl so we don't need to conditionally copy
-  mkdir ${DEPSDIR}/openssl/bin
-  cp amd64/lib* ${DEPSDIR}/openssl/bin/
-  
-  cd amd64
-  install_license LICENSE.txt openssl-3.0.15
+  OPENSSL_VER=3.0.15
 fi
+curl -L https://github.com/python/cpython-bin-deps/archive/refs/tags/openssl-bin-${OPENSSL_VER}.tar.gz --output cpython-bin-deps-openssl-bin-${OPENSSL_VER}.tar.gz
+tar -xf cpython-bin-deps-openssl-bin-${OPENSSL_VER}.tar.gz
+cd cpython-bin-deps-openssl-bin-${OPENSSL_VER}
+mkdir ${DEPSDIR}/openssl
+cp -r ${BINDEPS_ARCH}/include ${DEPSDIR}/openssl/include
+# cmake apparently wants this here?
+cp ${DEPSDIR}/openssl/include/applink.c ${DEPSDIR}/openssl/include/openssl/applink.c
+
+# required by cmake
+mkdir ${DEPSDIR}/openssl/lib
+cp ${BINDEPS_ARCH}/lib* ${DEPSDIR}/openssl/lib/
+
+# for compatibility with old openssl so we don't need to conditionally copy
+mkdir ${DEPSDIR}/openssl/bin
+cp ${BINDEPS_ARCH}/lib* ${DEPSDIR}/openssl/bin/
+  
+cd ${BINDEPS_ARCH}
+install_license LICENSE.txt openssl-${OPENSSL_VER}
 
 echo "::endgroup::"
 #########
@@ -61,7 +64,7 @@ cd bzip2
 mkdir build
 cd build
 cmake \
-  -G "Visual Studio 17 2022" -A x64 \
+  -G "Visual Studio 17 2022" -A ${CMAKE_ARCH} \
   -DCMAKE_INSTALL_PREFIX:PATH=${DEPSDIR}/bzip2 \
   ..
 cmake --build . --config Release -- /property:Configuration=Release
@@ -82,7 +85,7 @@ cd xz-5.4.5
 mkdir build
 cd build
 cmake \
-  -G "Visual Studio 17 2022" -A x64 \
+  -G "Visual Studio 17 2022" -A ${CMAKE_ARCH} \
   -DCMAKE_INSTALL_PREFIX:PATH=${DEPSDIR}/xz \
   ..
 cmake --build . --config Release -- /property:Configuration=Release
@@ -117,7 +120,7 @@ cd zlib-1.3.1
 mkdir build
 cd build
 cmake \
-  -G "Visual Studio 17 2022" -A x64 \
+  -G "Visual Studio 17 2022" -A ${CMAKE_ARCH} \
   -DCMAKE_INSTALL_PREFIX:PATH=${DEPSDIR}/zlib \
   ..
 cmake --build . --config Release -- /property:Configuration=Release
@@ -136,9 +139,9 @@ curl -L https://github.com/python/cpython-bin-deps/archive/refs/tags/libffi-3.4.
 tar -xf cpython-bin-deps-libffi-3.4.4.tar.gz
 cd cpython-bin-deps-libffi-3.4.4
 mkdir ${DEPSDIR}/libffi
-cp -r amd64/include ${DEPSDIR}/libffi/include
+cp -r ${BINDEPS_ARCH}/include ${DEPSDIR}/libffi/include
 mkdir ${DEPSDIR}/libffi/lib
-cp amd64/libffi* ${DEPSDIR}/libffi/lib/
+cp ${BINDEPS_ARCH}/libffi* ${DEPSDIR}/libffi/lib/
 install_license LICENSE libffi-3.4.4
 
 echo "::endgroup::"
@@ -154,13 +157,13 @@ if [[ "${DISTRIBUTION}" != "headless" ]]; then
   tar -xf cpython-bin-deps-tcltk-8.6.14.0.tar.gz
   cd cpython-bin-deps-tcltk-8.6.14.0
   mkdir ${DEPSDIR}/tcltk
-  cp -r amd64/include ${DEPSDIR}/tcltk/include
+  cp -r ${BINDEPS_ARCH}/include ${DEPSDIR}/tcltk/include
   mkdir ${DEPSDIR}/tcltk/lib
-  cp -r amd64/lib/* ${DEPSDIR}/tcltk/lib/
+  cp -r ${BINDEPS_ARCH}/lib/* ${DEPSDIR}/tcltk/lib/
   mkdir ${DEPSDIR}/tcltk/bin
-  cp amd64/bin/*.dll ${DEPSDIR}/tcltk/bin
-  install_license amd64/tcllicense.terms tcl-8.6.14.0
-  install_license amd64/tklicense.terms tk-8.6.14.0
+  cp ${BINDEPS_ARCH}/bin/*.dll ${DEPSDIR}/tcltk/bin
+  install_license ${BINDEPS_ARCH}/tcllicense.terms tcl-8.6.14.0
+  install_license ${BINDEPS_ARCH}/tklicense.terms tk-8.6.14.0
 
   echo "::endgroup::"
 fi
@@ -198,7 +201,7 @@ function build_python () {
   cmake \
     "${cmake_verbose_flags[@]}" \
     ${cmake_python_features} \
-    -G "Visual Studio 17 2022" -A x64 \
+    -G "Visual Studio 17 2022" -A ${CMAKE_ARCH} \
     -DPYTHON_VERSION=${PYTHON_FULL_VER} \
     -DPORTABLE_PYTHON_BUILD=ON \
     -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} \
